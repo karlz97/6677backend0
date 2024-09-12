53f793a2a50710dfd565076efd76ba55e8d87b50
karlz97 <yapiy@outlook.com>
2024-09-12 15:36:20 -0400

    remove confusing __main__

--- main.py
+++ main.py
@@ -10,9 +10,3 @@ app.include_router(router)
 @app.on_event("startup")
 async def startup_event():
     init_db()
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=80)
